0140d9f33737bdc382785d4f688f72c2d71a2c5a
MuhammadUsman.EE <usman.ee@outlook.com>
2022-09-24 21:04:20 +0200

    Navigation bar

--- lib/screens/main_dashboard/components/main_dashboard_body.dart
+++ lib/screens/main_dashboard/components/main_dashboard_body.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:rid_use/screens/main_dashboard/components/main_dashboard_background.dart';
+import 'package:rid_use/screens/welcome/welcome.dart';
 
 class MainDashboardBody extends StatelessWidget {
   const MainDashboardBody({
@@ -16,7 +17,7 @@ class MainDashboardBody extends StatelessWidget {
               child: ListView(
                 children: <Widget>[
                   DrawerHeader(
-                    child: Text(
+                    child: const Text(
                       "Drawer Text",
                       textAlign: TextAlign.justify,
                       textScaleFactor: 2.0,
@@ -24,33 +25,77 @@ class MainDashboardBody extends StatelessWidget {
                     decoration: BoxDecoration(color: Colors.green),
                   ),
                   ListTile(
-                    title: Text("First"),
+                    title: const Text("Profile"),
                     onTap: () {},
                   ),
                   ListTile(
-                    title: Text("Second"),
+                    title: const Text("Settings"),
+                    onTap: () {},
+                  ),
+                  ListTile(
+                    title: const Text("FAQs"),
                     onTap: () {},
                   )
                 ],
               ),
             ),
             appBar: AppBar(
-              title: Text("Rid-Use"),
-              bottom: const TabBar(tabs: [
+              leadingWidth: 100,
+              title: Container(
+                height: 40,
+                width: 40,
+                child: Image.asset("assets/images/mainLogoBlack.png"),
+              ),
+              bottom: TabBar(tabs: [
                 Tab(
-                  text: "Test",
+                  iconMargin: const EdgeInsets.only(
+                      left: 40, right: 40, bottom: 20, top: 20),
+                  child: Material(
+                    color: Colors.transparent,
+                    child: InkWell(
+                      child: Icon(Icons.dashboard_customize_outlined),
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => const Welcome()),
+                        );
+                      },
+                    ),
+                  ),
                 ),
                 Tab(
-                  text: "Test 2",
+                  icon: Icon(Icons.inventory),
+                  // text: "Inventory",
                 ),
                 Tab(
-                  text: "Test 3",
+                  icon: Icon(Icons.insights),
+                  // text: "Insights",
                 ),
                 Tab(
-                  text: "Tab 4",
+                  icon: Icon(Icons.contact_support),
+                  // text: "Support",
                 )
               ]),
             ),
+            body: TabBarView(children: [
+              Container(
+                alignment: Alignment.center,
+                child: const Text("Dashboard"),
+              ),
+              Container(
+                alignment: Alignment.center,
+                child: const Text("Inventory"),
+              ),
+              Container(
+                alignment: Alignment.center,
+                child: const Text("Insights"),
+              ),
+              Container(
+                alignment: Alignment.center,
+                child: const Text("Support"),
+              ),
+            ]),
           )),
     );
   }
