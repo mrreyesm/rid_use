20502cde227c8ddb7259982e343b7c6663e6989e
MuhammadUsman.EE <usman.ee@outlook.com>
2022-09-24 22:41:06 +0200

    Syncing

--- lib/screens/welcome/components/welcome_body.dart
+++ lib/screens/welcome/components/welcome_body.dart
@@ -1,23 +1,40 @@
 import 'package:flutter/material.dart';
 import 'package:rid_use/screens/main_dashboard/main_dashboard.dart';
 import 'package:rid_use/screens/welcome/components/welcome_background.dart';
+import 'package:rid_use/widgets/AppDrawer.dart';
+import 'package:rid_use/widgets/NavBar.dart';
 
 class WelcomeBody extends StatelessWidget {
   const WelcomeBody({
     Key? key,
   }) : super(key: key);
 
+  // @override
+  // Widget build(BuildContext context) {
+  //   return WelcomeBackground(
+  //       child: InkWell(
+  //       child: TextButton(
+  //         child: Text("Test"),
+  //         onPressed: () {Navigator.push(
+  //             context,
+  //             MaterialPageRoute(builder: (context) => const MainDashboard()),);},
+  //       ),
+  //     )
+  //   );
+  // }
+  // }
+
   @override
+  Size get preferredSize => const Size.fromHeight(100.0);
   Widget build(BuildContext context) {
     return WelcomeBackground(
-        child: InkWell(
-        child: TextButton(
-          child: Text("Test"),
-          onPressed: () {Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => const MainDashboard()),);},
-        ),
-      )
-    );
+        child: Scaffold(
+      backgroundColor: Colors.green,
+      appBar: PreferredSize(
+        preferredSize: preferredSize,
+        child: myAppBar("Dashboard"),
+      ),
+      drawer: myDrawer(),
+    ));
   }
-  }
+}
